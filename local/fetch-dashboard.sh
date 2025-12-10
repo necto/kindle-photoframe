5d7f78c5f063aa0660f52092dfda905e77489f88
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-12-10 20:02:37 +0100

    Report to the server the battery level on every image refresh

--- local/fetch-dashboard.sh
+++ local/fetch-dashboard.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 # Fetch a new dashboard image, make sure to output it to "$1".
 # For example:
-"$(dirname "$0")/../xh" -d -q -o "$1" get http://192.168.1.3:8080/image
+
+# Also report battery level to the image server
+# (since it is not displayed on the kindle itself)
+battery_level=$(gasgauge-info -c)
+"$(dirname "$0")/../xh" -d -q -o "$1" get http://192.168.1.3:8080/image?battery="$battery_level"

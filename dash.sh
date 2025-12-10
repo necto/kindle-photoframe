ec6f451be7ef96014038008240359c6b6fcfcf56
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-12-10 19:30:55 +0100

    Fix sleep duration

--- dash.sh
+++ dash.sh
@@ -108,7 +108,7 @@ rtc_sleep() {
     sleep "$duration"
   else
     # shellcheck disable=SC2039
-    [ -z "$(cat "$RTC_WAKE")" ] && echo $(($(cat "$RTC_TIME") + 20)) >"$RTC_WAKE"
+    [ -z "$(cat "$RTC_WAKE")" ] && echo $(($(cat "$RTC_TIME") + $duration)) >"$RTC_WAKE"
     echo "mem" >/sys/power/state
   fi
 }
